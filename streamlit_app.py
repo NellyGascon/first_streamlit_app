8fde4b53faefd5703f3216bf8a7b8fca8dfac4a8
NellyGascon <140792551+NellyGascon@users.noreply.github.com>
2023-08-01 19:16:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ if streamlit.button('Get Fruit Load List'):
 streamlit.header("View Our Fruit List - Add Your Favorites!")
 if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-   my_data_rows = get_fruit_load_list()
+   my_data_rows = get_fruit_list()
    my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
